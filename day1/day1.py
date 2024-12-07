a364012d72e150abbaff9eb755ed81175ebc7d1d
billyburrito <erinbritz@gmail.com>
2024-12-07 21:02:49 +0000

    day 1 done

--- day1/day1.py
+++ day1/day1.py
@@ -1,3 +1,27 @@
+first = []
+second = []
+data = []
+sum = 0
+part2 = 0
+
 with open('input') as fp:
     for line in fp:
-        print(line)
+        data = line.split()
+        first.append(data[0])
+        second.append(data[1])
+
+first.sort()
+second.sort()
+
+for x in range(len(first)):
+    if int(second[x]) > int(first[x]):
+        sum += int(second[x]) - int(first[x])
+    else:
+        sum += int(first[x]) - int(second[x])
+
+    # part 2
+    part2 += int(first[x]) * second.count(first[x])
+
+
+print("part one: "+str(sum))
+print("part two: "+str(part2))
